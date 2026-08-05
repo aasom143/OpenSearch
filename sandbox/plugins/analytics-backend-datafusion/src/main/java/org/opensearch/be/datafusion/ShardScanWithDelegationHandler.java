dadc5e5863003f12db08f3cd15f7f3bd0e1fdee2
Somesh Gupta <someshgupta987@gmail.com>
2026-08-05 15:55:14 +0530

    Force deleted_doc_filtering_required=true for testing
    Signed-off-by: Somesh Gupta <guptasom@amazon.com>

--- sandbox/plugins/analytics-backend-datafusion/src/main/java/org/opensearch/be/datafusion/ShardScanWithDelegationHandler.java
+++ sandbox/plugins/analytics-backend-datafusion/src/main/java/org/opensearch/be/datafusion/ShardScanWithDelegationHandler.java
@@ -77,7 +77,7 @@ public class ShardScanWithDelegationHandler implements FragmentInstructionHandle
                 delegatedPredicateCount,
                 node.requestsRowIds(),
                 context.hasPartialAggregate(),
-                node.requiresDeletedDocFiltering() && context.hasDeletedDocs(),
+                true,
                 segment.address(),
                 context.getFragmentBytes()
             );
