3a96cdd1f93a9ddc613cd9c573ba305d8c44b339
Somesh Gupta <someshgupta987@gmail.com>
2026-08-05 16:11:04 +0530

    Force deletedDocFilteringRequired=true for ListingTable path testing
    Signed-off-by: Somesh Gupta <guptasom@amazon.com>

--- sandbox/plugins/analytics-backend-datafusion/src/main/java/org/opensearch/be/datafusion/ShardScanInstructionHandler.java
+++ sandbox/plugins/analytics-backend-datafusion/src/main/java/org/opensearch/be/datafusion/ShardScanInstructionHandler.java
@@ -91,7 +91,7 @@ public class ShardScanInstructionHandler implements FragmentInstructionHandler<S
                     tableName,
                     contextId,
                     context.hasPartialAggregate(),
-                    context.hasDeletedDocs(),
+                    true,
                     segment.address(),
                     context.getFragmentBytes()
                 );
